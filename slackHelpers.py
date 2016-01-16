af42ccc091d89dfb711ecdb7aea00fa4a551ef79
Rob Dickinson <Rob Dickinson>
2016-01-16 12:44:39 +0000

    Added debug mode

--- slackHelpers.py
+++ slackHelpers.py
@@ -9,7 +9,12 @@ def sendToSlack(url, text, icon_url = None, icon_emoji = None):
 		data["icon_url"] = icon_url
 	if icon_emoji:
 		data["icon_emoji"] = icon_emoji
-	response = openUrl(url, json.dumps(data), headers = {"Content-type": "application/json"})
+
+	if DEBUG:
+		print (text.encode('utf-8'))
+		response = 200
+	else:
+		response = openUrl(url, json.dumps(data), headers = {"Content-type": "application/json"})
 
 	return response
 
@@ -19,7 +24,7 @@ def buildSlackStringFromTrain(train):
 	else:
 		lineStatus = "NOT on our line "
 
-	slackString =  "\n +++++++ \n" + lineStatus + "\n" + train["PlanDep"] + " (" + train["ActDep"] + ")" + "\n " + \
+	slackString =  "\n +++++++ \n" + lineStatus + "\n Planned: " + train["PlanDep"] + " (Actual: " + train["ActDep"] + ")" + "\n " + \
 					train["Origin"] +  " -> " + train["Destination"] + ".\n " + \
 					"Its ID is " + "<" + RTTBASEURL + train["IDLink"] + "|" + train["ID"] +">" + "\n +++++++ \n"
 	return slackString
